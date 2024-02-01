abc8eccee72862019aa3a48466bc9c32bac8f19a
vrajdesai78 <vrajdesai78@gmail.com>
2024-02-01 19:11:26 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.ALLOWED_ORIGIN,
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
