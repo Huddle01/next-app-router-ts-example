b9083503e0d7452f31f227e6fa34bcd32410f9f6
vrajdesai78 <vrajdesai78@gmail.com>
2024-02-01 19:31:24 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,26 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        // Routes this applies to
+        source: '/token/(.*)',
+        // Headers
         headers: [
+          // Allow for specific domains to have access or * for all
           {
             key: 'Access-Control-Allow-Origin',
-            value: process.env.ALLOWED_ORIGIN,
+            value: '*',
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
           },
+          // Allows for specific methods accepted
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
           },
         ],
       },
